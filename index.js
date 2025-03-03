f93ba644255c2cec3acc4619c212ab5ae61e2105
Taha Ghous <tahaghous2002@gmail.com>
2025-03-03 23:14:53 +0500

    use expressjs

--- index.js
+++ index.js
@@ -1,100 +1,16 @@
-import { createServer } from "http";
-import { readFile, writeFile } from "fs/promises";
-import path from "path";
-import crypto from "crypto";
+import express from "express";
+import { shortenerUrlRoutes } from "./routes/urlShortener.router.js";
+
+const app = express();
 
 const PORT = 3000;
 
-const DATA_FILE = path.join("data", "links.json");
+//Middleware
+app.use(express.urlencoded({ extended: true }));
 
-const loadLinks = async () => {
-  try {
-    const data = await readFile(DATA_FILE, "utf-8");
-    return JSON.parse(data);
-  } catch (error) {
-    if (error.code === "ENOENT") {
-      await writeFile(DATA_FILE, JSON.stringify({}));
-      return {};
-    }
-    throw error;
-  }
-};
+//Express Router
+app.use(shortenerUrlRoutes);
 
-const saveLinks = async (links) => {
-  try {
-    await writeFile(DATA_FILE, JSON.stringify(links, null, 2), "utf-8");
-  } catch (error) {
-    console.error("Error saving links:", error);
-  }
-};
-
-const server = createServer(async (req, res) => {
-  //GET REQUEST
-
-  if (req.method === "GET") {
-    if (req.url === "/") {
-      try {
-        const data = await readFile(path.join("public", "index.html"));
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.end(data);
-      } catch (error) {
-        res.writeHead(404, { "Content-Type": "text/plain" });
-        res.end("404 page not found");
-      }
-    } else if (req.url === "/links") {
-      const links = await loadLinks();
-      res.writeHead(200, { "Content-Type": "application/json" });
-      return res.end(JSON.stringify(links));
-    }
-  }
-
-  //POST REQUEST
-
-  if (req.method === "POST" && req.url === "/shortener") {
-    const links = await loadLinks();
-    let body = "";
-
-    req.on("data", (chunk) => {
-      body += chunk;
-    });
-
-    req.on("end", async () => {
-      try {
-        if (!body.trim()) {
-          throw new Error("Empty request body");
-        }
-        const { url, shortCode } = JSON.parse(body);
-
-        if (!url) {
-          res.writeHead(400, { "Content-Type": "application/json" });
-          return res.end(JSON.stringify({ error: "URL is required" }));
-        }
-
-        const finalShortCode =
-          shortCode || crypto.randomBytes(6).toString("hex");
-
-        if (links[finalShortCode]) {
-          res.writeHead(400, { "Content-Type": "application/json" });
-          return res.end(
-            JSON.stringify({
-              error: "Short code already exists. Please enter another.",
-            })
-          );
-        }
-
-        links[finalShortCode] = url;
-        await saveLinks(links);
-
-        res.writeHead(200, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ success: true, shortCode: finalShortCode }));
-      } catch (error) {
-        res.writeHead(400, { "Content-Type": "application/json" });
-        res.end(JSON.stringify({ error: "Invalid JSON input" }));
-      }
-    });
-  }
-});
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running at PORT ${PORT}`);
 });
